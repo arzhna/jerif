cb8fb7f53d0df7c9a0a528dd19530b5ba6d320fd
Arzhna Lee <arzhna@gmail.com>
2015-11-04 14:26:13 +0900

    rename inclusion guards

--- src/jerif.h
+++ src/jerif.h
@@ -1,8 +1,8 @@
-#ifndef __TOAST_PFLAT_JERIF_H__
-#define __TOAST_PFLAT_JERIF_H__
+#ifndef __JERIF_H__
+#define __JERIF_H__
 
 #include "jerif_types.h"
 
 jerif_err jerif_check_json_validation(const char* json_str);
 
-#endif //__TOAST_PFLAT_JERIF_H__
+#endif //__JERIF_H__
